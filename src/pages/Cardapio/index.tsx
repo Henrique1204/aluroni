225d8646f602f1414d2ffda1a70766c490a4bdd6
Paulo <pauloh16.pdr@gmail.com>
2022-06-03 23:12:17 -0300

    Criar o header e variaveis de estilo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
+import Styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
 const Cardapio: React.FC = () => {
-    return <main>
-        <nav className={Styles.menu}>
-            <Logo />
-        </nav>
-    </main>;
+  return (
+    <main>
+      <nav className={Styles.menu}>
+        <Logo />
+      </nav>
+
+      <header className={Styles.header}>
+          <div className={Styles.header__text}>
+              A casa do código e da Massa
+          </div>
+      </header>
+    </main>
+  );
 };
 
 export default Cardapio;
